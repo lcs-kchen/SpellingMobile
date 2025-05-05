3ea43ea32b9a50ef405aeff67ca9109b5c45d22b
Kevin Chen <kchen@lcs.on.ca>
2025-05-05 12:20:20 -0400

    Finished defining the function (teaching the computer how to filter the array based on outcome).

--- SpellingMobile/Functions/Filtering.swift
+++ SpellingMobile/Functions/Filtering.swift
@@ -18,6 +18,40 @@ func filtering(
     originalList: [Result],
     on desiredOutcome: Outcome
 ) -> [Result] {
-    //return an empty array
-    return []
+    //Sequence (steps in the correct order)
+    
+    //Selection
+    // 0. See if the user selected "All results"
+    if desiredOutcome == .undetermined {
+        return originalList
+    } else {
+        
+        // 1. Make a new empty array
+        var filteredHistory: [Result] = []
+        
+        // 2. Iterate over each item
+        //    in the original array and
+        //    decide whether to copy it to
+        //    the filtered array
+        //
+        //    "foodItem" points at each
+        //    element of the array in turn
+        for foodItem in originalList {
+            
+            // 3. Decide whether to copy over
+            //
+            // e.g.: say "desiredOutcome" is
+            // "incorrect"... we copy over only
+            // food items where the user made
+            // the wrong guess
+            if foodItem.outcome == desiredOutcome {
+                filteredHistory.append(foodItem)
+            }
+        }
+        
+        // 4. Return the filtered array
+        return filteredHistory
+        
+    }
+    
 }
